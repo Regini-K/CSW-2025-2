82b61335d67354a345980a3858a7c84e79bb617f
Khrystopher Regini <khrystopher.edu@gmail.com>
2025-11-11 20:39:43 -0300

    Add files via upload

---
+++ Aula16/calculadora.js
@@ -0,0 +1,27 @@
+        function calculaIdade() {
+            var dia = document.getElementById("dia_nascimento").value;
+            var mes = document.getElementById("mes_nascimento").value;
+            var ano = document.getElementById("ano_nascimento").value;
+
+            dataAtual = new Date();
+
+            var diaA = dataAtual.getDate();
+            var mesA = dataAtual.getMonth() + 1;
+            var anoA = dataAtual.getFullYear();
+
+            if(dia <= 0 || dia > 31 || ano<= 0){
+                alert("Digite uma data de nascimento válida")
+                return
+            }
+
+            usuarioAno = anoA - ano;
+
+            if(mes > mesA){
+                usuarioAno--;
+            }
+            else if((mes == mesA) && (dia > diaA)){
+                usuarioAno--;
+            }
+
+            document.getElementById('resposta').innerHTML = 'Data de nascimento: '+ dia + '/' + mes + '/' + ano + '<br>' + 'Você tem: ' + usuarioAno + ' anos' + '<br>' + 'Data de hoje: ' + diaA + '/' + mesA + '/' + anoA;
+        }
